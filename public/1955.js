361e1173e23daa502f53d23ea02eafb4c1a0e990
mogi <mogi@mogiui-noteubug.local>
2024-04-09 15:46:31 +0900

    init

---
+++ public/1955.js
@@ -0,0 +1,159 @@
+r89.adunits.performance = {
+    "Shorturlat-Desktop-Leaderboard-BTF": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ],
+        "au_cb": [
+            1,
+            2,
+            3
+        ]
+    },
+    "Shorturlat-Mobile-Rectangle-Mid": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4
+        ],
+        "au_cb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        ]
+    },
+    "Shorturlat-Desktop-Pushup": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        ],
+        "au_cb": [
+            1
+        ]
+    },
+    "Shorturlat-Desktop-Leaderboard-ATF": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6
+        ],
+        "au_cb": [
+            1
+        ]
+    },
+    "Shorturlat-Desktop-HPA-ATF-Left": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        ],
+        "au_cb": [
+            1
+        ]
+    },
+    "Shorturlat-Desktop-HPA-ATF-Right": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        ],
+        "au_cb": [
+            1,
+            2
+        ]
+    },
+    "Shorturlat-Interstitial": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ],
+        "au_cb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        ]
+    },
+    "Shorturlat-Mobile-Rectangle-Infinite": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5
+        ],
+        "au_cb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
+        ]
+    },
+    "Shorturlat-Mobile-pushup": {
+        "au_vb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+        ],
+        "au_cb": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        ]
+    }
+};
